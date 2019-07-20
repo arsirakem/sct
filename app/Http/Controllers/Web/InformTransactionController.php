de9d873771325a25b3845234aab8c1ac1fa06902
arsirakem <arsirakem@gmail.com>
2019-07-20 16:44:33 +0700

    Edit IT - PP with Controller
    Problem with extracting the data from database (Controller file)

--- app/Http/Controllers/Web/InformTransactionController.php
+++ app/Http/Controllers/Web/InformTransactionController.php
@@ -6,16 +6,8 @@ use Vanguard\Http\Controllers\Controller;
 
 class InformTransactionController extends Controller
 {
-    public function __construct()
-    {
-        // Allow access to authenticated users only.
-        $this->middleware('auth');
-
-        // Allow access to users with 'users.manage' permission.
-    }
-
     public function index()
     {
-        return "Hello from ActiveUsersController";
+        return view('user.inform-transaction');   
     }
 }
